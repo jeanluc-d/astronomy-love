f0eb064adccbecefc043ce2a99ea7a96511e4115
Jean-Luc Desjardins <70818495+JLDCodes@users.noreply.github.com>
2022-08-10 20:21:16 -0400

    add comment in custom hook

--- client/src/hooks/useLocalStorageState/useLocalStorageState.js
+++ client/src/hooks/useLocalStorageState/useLocalStorageState.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 
 function useLocalStorageState(key, defaultValue) {
+  /* use callback to avoid reading from localStorage every time 
+   * avoid performance issues
+  */
   const [state, setState] = useState(() => {
     try {
       const item = localStorage.getItem(key);
